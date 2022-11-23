aedb16a4c4cf3d712da5e91913ee38d4bb02f4a0
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2022-11-23 10:01:41 -0500

    adding models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { IndexModule } from "./components/index.module";
@@ -13,9 +13,13 @@ import { IndexComponent} from "./components/index.component";
     AppComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     FormsModule,
-    IndexModule
+    IndexModule,
+    RouterModule.forRoot([
+      { path: '', component: IndexComponent },
+      { path: '**', redirectTo: '' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
