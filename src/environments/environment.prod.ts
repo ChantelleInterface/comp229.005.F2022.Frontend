60d5bd5de4a113b166c5e1ab8de178aff73a1176
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2023-01-05 14:22:26 -0500

    edits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
   apiURL: "https://helical-realm-373207.nn.r.appspot.com/",
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: "AIzaSyAZ4n945_YTn08pEOIOsOnl2uV_BgUKKhA",
+    authDomain: "helical-realm-373207.firebaseapp.com",
+    projectId: "helical-realm-373207",
+    storageBucket: "helical-realm-373207.appspot.com",
+    messagingSenderId: "597952420940",
+    appId: "1:597952420940:web:1f50fc4176ce89522f28e9"
+  }
 };
