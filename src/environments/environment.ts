4d09fcc1fe5bdc7f03fdc8e01ef9807d1028362e
Chantelle Lawson <clawso11@my.centennialcollege.ca>
2023-01-06 21:33:05 -0500

    edits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiURL: "http://localhost:3000/",
-  firebaseConfig: "process.env.firebaseConfig"
+  firebaseConfig: {}
 };
 /*
  * For easier debugging in development mode, you can import the following file
